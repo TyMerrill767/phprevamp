0565f4c602ac4e5b7f95ecd6c98c1a673973c3fe
Tyler Merrill <tyler_merrill2011@hotmail.com>
2015-04-09 09:06:45 -0500

    Replacing index.html with index.php; first PHP include, header.php

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<nav class="nav" role="navigation">
+    <img class="nav-logo" src="_img/daft-punk-logo_light.png" alt="logo" title="daft punk logo">
+    <ul>
+        <li><a href="#band">The Band</a></li>
+        <li><a href="#albums">Albums</a></li>
+        <li><a href="#mail">Mailing List</a></li>
+    </ul>
+</nav>
--- index.php
+++ index.php
@@ -19,15 +19,7 @@
     </head>
 
 <body>
-
-    <nav class="nav" role="navigation">
-        <img class="nav-logo" src="_img/daft-punk-logo_light.png" alt="logo" title="daft punk logo">
-        <ul>
-            <li><a href="#band">The Band</a></li>
-            <li><a href="#albums">Albums</a></li>
-            <li><a href="#mail">Mailing List</a></li>
-        </ul>
-    </nav>
+    <?php include('includes/header.php'); ?>
 
     <div class="container-fluid jumbotron main">
         <img class ="jumbo-logo" src="_img/daft-punk-logo_gold.png" alt="logo" title="daft punk logo">
