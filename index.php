a7b6dcb03124aa5183722f46eec4d05ef1a3e6d0
Tyler Merrill <tyler_merrill2011@hotmail.com>
2015-04-09 09:37:17 -0500

    Adding footer.php to includes.

--- index.php
+++ index.php
@@ -181,18 +181,7 @@
             </div>
             </form>
         </div>
-
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer">
-        <footer>
-            <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12">
-                <p class="footer-copy">&copy; Luke Atkinson.<br> This site made for educational purposes.</p>
-            </div>
-            <div class="col-lg-9 col-md-12 col-sm-12 col-xs-12">
-                <p>Images found via Google Image search. Artist bio sourced from Wikipedia.<br> Script based on &quot;<a href="http://codyhouse.co/gem/css-animated-headlines/">Animated Headlines</a>&quot; by CodyHouse.co. </p>
-            </div>
-        </footer>
-    </div>
-
+    <?php include('includes/footer.php'); ?>
 
 </body>
 
