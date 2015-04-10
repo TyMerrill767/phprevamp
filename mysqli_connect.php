e00896fc3a4c6a534a99d9335010835e09285fc0
Tyler Merrill <tyler_merrill2011@hotmail.com>
2015-04-10 08:56:52 -0500

    Making correction to mysqli_connect.php.

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -2,8 +2,9 @@
 DEFINE ('DB_USER', 'admin');
 DEFINE ('DB_PASSWORD', 'instructor');
 DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'phpredisocgraphy');
+DEFINE ('DB_NAME', 'phprediscography');
 
 $dbcon = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL : ' . mysqli_connect_error() );
 
 mysqli_set_charset($dbcon, 'utf8');
+?>
