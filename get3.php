d82bcd44d4da56a77757b53e5b5cb29f73f10916
Tyler Merrill <tyler_merrill2011@hotmail.com>
2015-04-23 10:16:23 -0500

    Attempting different method of outputting values from database.

--- get3.php
+++ get3.php
@@ -1,8 +1,39 @@
-<?php
-        require('mysqli_connect.php');
-        $q = "SELECT albumname, coverart
-              FROM albums
-              INNER JOIN tracks ON albums.albumID = tracks.albumID";
-        $t = "SELECT trackname FROM tracks WHERE albumId = 1";
-        $b = "SELECT trackname FROM tracks WHERE albumID = 2";
-?>
+	<?php
+    require('mysqli_connect.php');
+
+	// Connect to database server
+	mysql_connect("localhost", "admin", "instructor") or die (mysql_error ());
+
+	// Select database
+	mysql_select_db("phprediscography") or die(mysql_error());
+
+	// SQL query
+	$q = "SELECT *
+    FROM albums
+    JOIN tracks
+    ON albums.albumID=tracks.albumID";
+    $t = "SELECT trackname FROM tracks WHERE Album_id = 1";
+    $b = "SELECT trackname FROM tracks WHERE Album_id = 2";
+    $n = "SELECT trackname FROM tracks WHERE Album_id = 3";
+    $r = "SELECT trackname FROM tracks WHERE Album_id = 4";
+    $result = mysqli_query($dbcon, $q);
+    $result1 = mysqli_query($dbcon, $t);
+    $result2 = mysqli_query($dbcon, $b);
+    $result3 = mysqli_query($dbcon, $n);
+    $result4 = mysqli_query($dbcon, $r);
+    if ($result)
+    {
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+        {
+               if($row['albumID']==1){
+                    foreach($result1 as $row1) {
+                        //echo '<img src='.$row['coverart'] . '>';
+                        //echo $row['albumname'] . "<br />";
+                        echo '<li>' . $row1['trackname'] . '</li>';
+                    }
+               }
+        }
+    }
+	// Close the database connection
+	mysql_close();
+	?>
