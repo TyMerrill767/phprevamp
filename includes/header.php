5f5d351656320bb98251dac5c29f1bebd395c709
Tyler Merrill <tyler_merrill2011@hotmail.com>
2015-05-14 09:44:09 -0500

    Adding login link and styling for it.

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,8 @@
 <nav class="nav" role="navigation">
     <img class="nav-logo" src="_img/daft-punk-logo_light.png" alt="logo" title="daft punk logo">
+    <ul class="login">
+        <li><a href="admin-login.php">Login</a></li>
+    </ul>
     <ul>
         <li><a href="#band">The Band</a></li>
         <li><a href="#albums">Albums</a></li>
