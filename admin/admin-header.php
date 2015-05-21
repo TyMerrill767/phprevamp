c192b029722bc7fc17617a956a0a82c46ededb6e
Tyler Merrill <tyler_merrill2011@hotmail.com>
2015-05-21 10:35:53 -0500

    Committing updated admin-header.php as well as new admin-logout.php.

--- admin/admin-header.php
+++ admin/admin-header.php
@@ -1,8 +1,8 @@
 <nav class="nav" role="navigation">
     <img class="nav-logo" src="_img/daft-punk-logo_light.png" alt="logo" title="daft punk logo">
     <ul class="login">
-        <li><a href="admin-login2.php">Login</a></li>
-        <li><a href=""></a></li>
+        <li><a href="admin/admin-logout.php">Logout</a></li>
+        <li><a href="admin/addalbum.php">Add An Album</a></li>
     </ul>
     <p>Hello, Christy</p>
     <ul>
